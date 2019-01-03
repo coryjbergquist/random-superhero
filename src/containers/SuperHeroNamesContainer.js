16c7746d8e8e157e73e2115609e466f86dd35ab5
coryjbergquist <cory.j.bergquist@gmail.com>
2019-01-02 17:26:36 -0700

    add name list to state of SuperHeroNamesContainer

--- src/containers/SuperHeroNamesContainer.js
+++ src/containers/SuperHeroNamesContainer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 class SuperHeroNamesContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {names: ["superman", "batman", "thor", "iron man", "dr. strange", "spiderman", "aquaman"]}
+  }
 
   render() {
     return (
       <p>
-        yo
+        {this.state.names[Math.floor(Math.random() * this.state.names.length)]}
       </p>
     )
   }
